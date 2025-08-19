2c27521cb432660594ac068162066806289302a8
TimothyCruzCk12 <timothy.cruz.intern@ck12.org>
2025-08-19 11:50:21 -0700

    added app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MeasuringInches from './components/MeasuringInches';
+
+
+function App() {
+  return (
+      <div className="App">
+        <MeasuringInches />
+      </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/MeasuringInches.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+const MeasuringInches = () => {
+
+	return (
+        <div>test</div>
+)
+};
+
+
+export default MeasuringInches;
